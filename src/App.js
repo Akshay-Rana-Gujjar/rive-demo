0f1eaaccc283b5b6ac4163926f5f17c0c54e3d96
Akshay Rana <akshaybhati28@gmail.com>
2022-04-11 23:55:06 +0530

    custom animation added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import Rive, { useRive } from "rive-react";
 
 function App() {
+  const params = {
+    src: require("./assets/rive-src/name_animation.riv"),
+    autoplay: false,
+  };
+  const { RiveComponent, rive } = useRive(params);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <Rive src={require("./assets/rive-src/menu_animation.riv")} />
+        <small >Hover on below image</small>
+        <div style={{display: "flex"}}>
+          <RiveComponent
+            onMouseEnter={() => rive && rive.play()}
+            onMouseLeave={() => rive && rive.pause()}
+          />
+
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Animation using Rive.js
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.npmjs.com/package/rive-react"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Rive
         </a>
       </header>
     </div>
